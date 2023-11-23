4ba0390195f0ec90b69b968dd2dc4d4d44f9a029
wei_an <wei_an@phalanity.com.tw>
2023-11-23 16:23:46 +0800

    讀取結構調整

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -16,7 +16,7 @@ public class Main : MonoBehaviour
     /// 
     /// </summary>
     [SerializeField]
-    List<DataStruct> Data = new List<DataStruct>();
+    List<DMXDataStruct> Data = new List<DMXDataStruct>();
 
     /// <summary>
     /// 
@@ -36,7 +36,7 @@ public class Main : MonoBehaviour
     Text Txt_Time2;
 
     // Start is called before the first frame update
-    IEnumerator Start()
+    void Start()
     {
         using (var Stream = File.Open(Application.streamingAssetsPath + "/Test2.xlsx", FileMode.Open, FileAccess.Read))
         {
@@ -44,53 +44,98 @@ public class Main : MonoBehaviour
             {
                 var Result = Reader.AsDataSet();
 
+                Debug.LogWarning("資料表數量 : " + Result.Tables.Count);
+                foreach (var Table in Result.Tables)
+                {
+                    //Debug.Log(Table);
+
+                    DataRowCollection DataRow = Result.Tables[Table.ToString()].Rows;
+                    //Debug.LogWarning("Rows : " + DataRow.Count);
+
+                    DMXDataStruct Data = new DMXDataStruct(short.Parse(DataRow[0].ItemArray[1].ToString()));
+
+                    #region 記下資料要到哪裡中止讀取
+                    int DmxEnd = 0;
+                    for (int a = 3; a < DataRow[0].ItemArray.Length; a++)
+                    {
+                        if (int.TryParse(DataRow[0].ItemArray[a].ToString(), out int DMXID))
+                        {
+                            Debug.Log("DMX ID : " + DMXID);
+                        }
+                        else
+                        {
+                            DmxEnd = a;
+                            break;
+                        }
+                    }
+                    #endregion
+
+                    //foreach (DataRow Row in DataRow)
+                    //{
+                    //    //Debug.Log(string.Join(", ", Row.ItemArray));
+
+                    //    //設Universe
+                    //    if (Row.ItemArray[0].ToString() == "Universe")
+                    //    {
+                    //        //Debug.Log("Universe : " + Row.ItemArray[1]);
+
+                    //        Data = new DMXDataStruct(short.Parse(Row.ItemArray[1].ToString()));
+
+                    //        for (int a = 3; a < Row.ItemArray.Length; a++)
+                    //        {
+                    //            if (int.TryParse(Row.ItemArray[a].ToString(), out int DMXID))
+                    //            {
+                    //                Debug.Log("DMX ID : " + DMXID);
+                    //            }
+                    //            else
+                    //            {
+                    //                DmxEnd = a;
+                    //                break;
+                    //            }
+                    //        }
+                    //    }
+
+
+
+                    //    //if (TimeSpan.TryParseExact(Row.ItemArray[1].ToString(), new string[] { @"mm\:ss", @"m\:ss" }, null, out TimeSpan ActionTime))
+                    //    //{
+                    //    //    Data.Add(new DataStruct(ActionTime, new float[] {
+                    //    //        float.Parse(Row.ItemArray[2].ToString()) ,
+                    //    //        float.Parse(Row.ItemArray[3].ToString()) ,
+                    //    //        float.Parse(Row.ItemArray[4].ToString()) ,
+                    //    //        float.Parse(Row.ItemArray[5].ToString()),
+                    //    //        float.Parse(Row.ItemArray[6].ToString()),
+                    //    //        float.Parse(Row.ItemArray[7].ToString())
+                    //    //    }));
+
+                    //    //    //Debug.Log(ID + " ActionTime : " + ActionTime.TotalSeconds + " (" + ActionTime.Minutes.ToString("00") + ":" + ActionTime.Seconds.ToString("00") + ")");
+
+                    //    //    //ID++;
+                    //    //}
+                    //}
+
+                    #region 讀取時間與數值
+                    for (int a = 2; a < DmxEnd; a++)//每一行
+                    {
+                        Debug.LogWarning(string.Join(", ", DataRow[a].ItemArray));
+
+                        for (int b = 0; b < DmxEnd; b++)
+                        {
+                            Debug.Log(a + "-" + b + " : " + DataRow[a].ItemArray[b]);
+                        }
+                    }
+                    #endregion
+
+
+                }
+
                 //var ID = 0;
 
-                DataRowCollection DataRow = Result.Tables["工作表1"].Rows;
-                //Debug.LogWarning("Rows : " + DataRow.Count);
 
-                foreach (DataRow Row in DataRow)
-                {
-                    //Debug.LogWarning(string.Join(", ", Row.ItemArray));
-                    if (TimeSpan.TryParseExact(Row.ItemArray[1].ToString(), new string[] { @"mm\:ss", @"m\:ss" }, null, out TimeSpan ActionTime))
-                    {
-                        Data.Add(new DataStruct(ActionTime, new float[] {
-                            float.Parse(Row.ItemArray[2].ToString()) ,
-                            float.Parse(Row.ItemArray[3].ToString()) ,
-                            float.Parse(Row.ItemArray[4].ToString()) ,
-                            float.Parse(Row.ItemArray[5].ToString()),
-                            float.Parse(Row.ItemArray[6].ToString()),
-                            float.Parse(Row.ItemArray[7].ToString())
-                        }));
-
-                        //Debug.Log(ID + " ActionTime : " + ActionTime.TotalSeconds + " (" + ActionTime.Minutes.ToString("00") + ":" + ActionTime.Seconds.ToString("00") + ")");
-
-                        //ID++;
-                    }
-                }
 
             }
         }
 
-        DateTime StartTime = DateTime.Now;
-        //for (int a = 0; a < Data.Count; a++)
-        //{
-        //    yield return new WaitUntil(() => StartTime + Data[a].ActionTime >= DateTime.Now);
-
-        //    //Debug.Log(a + " : " + string.Join(", " + Data[a].FanArray));
-        //    Debug.Log
-        //}
-
-        int ID = 0;
-        while (true)
-        {
-            //if (StartTime + Data[ID].ActionTime >= DateTime.Now && !Data[ID].IsTrigger)
-            //{
-            //    Debug.Log()
-            //}
-
-            yield return null;
-        }
     }
 
     // Update is called once per frame
@@ -119,20 +164,54 @@ public class Main : MonoBehaviour
     }
 }
 
+/// <summary>
+/// 
+/// </summary>
 [Serializable]
-struct DataStruct
+struct DMXDataStruct
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    public short Universe;
+    /// <summary>
+    /// 
+    /// </summary>
+    public DMXValueStruct[] Values;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="Universe"></param>
+    public DMXDataStruct(short Universe)
+    {
+        this.Universe = Universe;
+        Values = new DMXValueStruct[512];
+    }
+}
+/// <summary>
+/// 
+/// </summary>
+[Serializable]
+struct DMXValueStruct
+{
+    /// <summary>
+    /// 
+    /// </summary>
     public TimeSpan ActionTime;
-    public float[] FanArray;
+    /// <summary>
+    /// 
+    /// </summary>
+    public float Value;
     /// <summary>
     /// 是否已觸發
     /// </summary>
     public bool IsTrigger;
 
-    public DataStruct(TimeSpan ActionTime, float[] FanArray)
+    public DMXValueStruct(TimeSpan ActionTime, float Value)
     {
         this.ActionTime = ActionTime;
-        this.FanArray = FanArray;
+        this.Value = Value;
         IsTrigger = false;
     }
 }
